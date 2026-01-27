718f681220d59bceb48750bab123230b172977ff
Luis Castro <luiscastrosounds@gmail.com>
2026-01-27 01:21:24 +0000

    Fixed protected routes problem with logout (only worked after refresh)

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -38,6 +38,8 @@ const AuthWrapper = ({ children }) => {
   }
   const handleLogout = () => {
     localStorage.removeItem("authToken");
+    setCurrentUser(null);
+    setIsLoggedIn(false);
     nav("/login");
   };
 
